9701df441ddb79f02f7715997ae37d3e53293f1e
Esaú Freitas <esau@funifier.com>
2022-01-22 16:55:14 -0300

    initial commit, project structure

---
+++ src/main/java/com/wbip/indicators/EconomicIndicatorsOfPovertyApplication.java
@@ -0,0 +1,13 @@
+package com.wbip.indicators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomicIndicatorsOfPovertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomicIndicatorsOfPovertyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wbip/indicators/EconomicIndicatorsOfPovertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbip.indicators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomicIndicatorsOfPovertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
